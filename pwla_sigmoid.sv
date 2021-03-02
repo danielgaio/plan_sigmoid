46f02c7955ecf74504aada33f61f393920b5e5f0
Daniel <danielelielgaio@gmail.com>
2021-03-01 21:37:58 -0300

    Sync
    Começando a implementar a equação por partes PWLA.

--- pwla_sigmoid.sv
+++ pwla_sigmoid.sv
@@ -7,6 +7,14 @@ module pwla_sigmoid (input [15:0]x, output [15:0]f_x);
 	initial begin
 		x_int = x * 1024;
 		$display(x_int);
+		
+		// implementação da equação por partes
+		// nesse momento considerar que o x de entrada é sempre positivo
+		if (x_int >= 5120) begin
+			f_x = 1024;
+		end else if (x_int >= 2432 & x_int < 5120) begin
+			f_x = (x_int + 864) >> 5;
+		end
 	end
 
 endmodule
